d6b6deff96a2bbe3db91b3b8e75103f4617b865d
XOfSpades <stoecki@der-ball-ist-rund.net>
2017-10-11 22:16:14 +0200

    Set version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule SimpleRepo.Mixfile do
   use Mix.Project
 
   def project do
-    version = "1.0.0-rc1"
+    version = "1.0.0"
     [app: :simple_repo,
      version: version,
      elixir: "~> 1.5",
