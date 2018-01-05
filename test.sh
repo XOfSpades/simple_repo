90024393d1b79424220dc2e0ac866a839311163f
XOfSpades <stoecki@der-ball-ist-rund.net>
2018-01-05 21:04:24 +0100

    Ensure fast test failing on compile error

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker-compose up -d --build simple-repo-postgres-test-s
+docker-compose build simple-repo-test-s || exit
 
-docker-compose build simple-repo-test-s
+docker-compose up -d --build simple-repo-postgres-test-s
 
 docker-compose run simple-repo-test-s
 
