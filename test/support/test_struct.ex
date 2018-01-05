35acca61c2dc9e4ec7ee00445e540dee070bcfa4
XOfSpades <stoecki@der-ball-ist-rund.net>
2018-01-05 21:28:43 +0100

    Allow any kind of number in query

--- test/support/test_struct.ex
+++ test/support/test_struct.ex
@@ -5,15 +5,16 @@ defmodule SimpleRepo.Support.TestStruct do
   use Ecto.Schema
 
   schema "test_structs" do
-    field :name,  :string
-    field :type,  :string
-    field :value, :integer
+    field :name,    :string
+    field :type,    :string
+    field :value,   :integer
+    field :f_value, :float
 
     timestamps()
   end
 
   def changeset(model, params \\ :empty) do
-    fields = ~w(name type value)
+    fields = ~w(name type value f_value)
 
     model
     |> Ecto.Changeset.cast(params, fields)
