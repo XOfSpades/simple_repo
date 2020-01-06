885c8f3312baf4d67692c489fd29479e5b17f0f9
bernhard <bernhard.stoecker@recogizer.de>
2020-01-06 10:02:49 +0100

    Update dependencies

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -2,6 +2,6 @@ defmodule SimpleRepo.Support.Repo do
   @moduledoc """
   Only used for testing purpose.
   """
-  use Ecto.Repo, otp_app: :simple_repo
+  use Ecto.Repo, otp_app: :simple_repo, adapter: Ecto.Adapters.Postgres
   use SimpleRepo.Scoped, repo: __MODULE__
 end
